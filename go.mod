56a4e3001be4813777c7628f77680a7c18e339cc
kotekaman <abdul.haris.djafar@gmail.com>
2021-06-13 08:48:13 +0700

    new goroutine

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/gin-gonic/gin v1.7.2
 	go.mongodb.org/mongo-driver v1.5.3
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 )
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -14,6 +15,54 @@ var (
 
 func main() {
 	r := gin.Default()
+	r.POST("/save/goroutine", func(cTemp *gin.Context) {
+		var data entity.Trainer
+		quit := make(chan bool)
+		errc := make(chan error)
+		done := make(chan error)
+
+		c := cTemp.Copy()
+
+		go func() {
+			ginChannel := done
+			errs := error(nil)
+
+			err := c.ShouldBindJSON(&data)
+			passwordAfterEncrypt, err := bcrypt.GenerateFromPassword([]byte(data.Passwords), bcrypt.DefaultCost)
+			data.Passwords = string(passwordAfterEncrypt)
+
+			err = trainerRepo.Save(data)
+
+			if err != nil {
+				errs = err
+			}
+
+			if errs != nil {
+				ginChannel = errc
+			}
+
+			select {
+			case ginChannel <- err:
+				return
+			case <-quit:
+				return
+			}
+		}()
+
+		select {
+		case err := <-errc:
+			close(quit)
+			cTemp.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+
+		case <-done:
+			cTemp.JSON(200, gin.H{
+				"IsError": false,
+				"code":    200,
+				"data":    data,
+			})
+		}
+
+	})
 	r.POST("/save", func(c *gin.Context) {
 		var data entity.Trainer
 
@@ -21,6 +70,8 @@ func main() {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
+		passwordAfterEncrypt, _ := bcrypt.GenerateFromPassword([]byte(data.Passwords), bcrypt.DefaultCost)
+		data.Passwords = string(passwordAfterEncrypt)
 
 		err := trainerRepo.Save(data)
 
@@ -30,8 +81,11 @@ func main() {
 		}
 
 		c.JSON(200, gin.H{
-			"message": "success",
+			"IsError": false,
+			"code":    200,
+			"data":    data,
 		})
+
 	})
 	r.Run()
 }
