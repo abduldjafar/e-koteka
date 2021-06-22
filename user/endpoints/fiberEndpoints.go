659b54cf9c33a74163ea2d584a4218efd4080bb4
kotekaman <abdul.haris.djafar@gmail.com>
2021-06-22 07:53:17 +0700

    update fiber endpoint

--- user/endpoints/fiberEndpoints.go
+++ user/endpoints/fiberEndpoints.go
@@ -1,9 +1,11 @@
 package endpoints
 
 import (
+	"os"
 	"user/controller"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 type fiberEndpoints struct{}
@@ -21,6 +23,10 @@ func (*fiberEndpoints) ALL() interface{} {
 	fiberRouter.Post("/v1/users", fiberUserController.AddUser().(fiber.Handler))
 	fiberRouter.Get("/v1/users/all", fiberUserController.GetAllUsers().(fiber.Handler))
 	fiberRouter.Get("/v1/users/:id", fiberUserController.GetUser().(fiber.Handler))
+	fiberRouter.Use(logger.New(logger.Config{
+		Format: "${pid} ${locals:requestid} ${status} - ${method} ${path}\n",
+		Output: os.Stdout,
+	}))
 
 	return fiberRouter
 }
